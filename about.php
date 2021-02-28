d60dfa1da5eee0f5f45ce13b76cc291560969468
amandabriena <amanda.briena@gmail.com>
2021-02-27 23:54:03 -0300

    atualizacao pagina quem somos

---
+++ about.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?php require_once("src/components/head.php");?>
+	<!-- title -->
+	<title>Quem somos</title>
+
+</head>
+<body>
+	
+	<!--PreLoader-->
+    <div class="loader">
+        <div class="loader-inner">
+            <div class="circle"></div>
+        </div>
+    </div>
+    <!--PreLoader Ends-->
+	<!--MENU-->
+	<?php require_once("src/components/menu.php");?>
+
+	<!-- breadcrumb-section -->
+	<div class="breadcrumb-section breadcrumb-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="breadcrumb-text">
+						<p>COOBAF-FS</p>
+						<h1>Quem Somos</h1>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end breadcrumb section -->
+
+	<!-- featured section -->
+	<div class="feature-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-7">
+					<div class="featured-text">
+						<h2 class="pb-3">A <span class="orange-text">COOBAF-FS</span></h2>
+						<div class="row">
+							<div class="col-lg-6 col-md-6 mb-4 mb-md-5">
+								<div class="list-box d-flex">
+									<div class="list-icon">
+										<i class="fas fa-shipping-fast"></i>
+									</div>
+									<div class="content">
+										<h3>Agricultura Familiar</h3>
+										<p>X</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-6 mb-5 mb-md-5">
+								<div class="list-box d-flex">
+									<div class="list-icon">
+										<i class="fas fa-money-bill-alt"></i>
+									</div>
+									<div class="content">
+										<h3>Pequenos Comerciantes</h3>
+										<p>X</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-6 mb-5 mb-md-5">
+								<div class="list-box d-flex">
+									<div class="list-icon">
+										<i class="fas fa-briefcase"></i>
+									</div>
+									<div class="content">
+										<h3>Menos Agrotóxicos</h3>
+										<p>X</p>
+									</div>
+								</div>
+							</div>
+							<div class="col-lg-6 col-md-6">
+								<div class="list-box d-flex">
+									<div class="list-icon">
+										<i class="fas fa-sync-alt"></i>
+									</div>
+									<div class="content">
+										<h3>Fortalecendo a Região</h3>
+										<p>X</p>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end featured section -->
+
+	<!-- HISTORIA -->
+	<div class="mt-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="section-title">
+						<h3>Nossa <span class="orange-text">História</span></h3>
+						<h5 class="text-justify"> 
+						A Cooperativa de Beneficiamento e Comercialização de Produtos da Agricultura Familiar de Feira de Santana (COOBAF-FS)
+						foi fundada em 20XX, a partir da união de associações rurais da região.</h5>
+						<h5>
+						A COOBAF/FS é uma cooperativa de trabalhadores/as rurais de Feira de Santana (2º maior município da Bahia e 2º maior entroncamento rodoviário do país),
+						composta por 87 membros, em sua maioria mulheres, oriundos de todos os oito distritos rurais do município. A sua irradiação nos quatro cantos
+						da zona rural de Feira de Santana é parte de sua ação integrada, que conta com apoio de Associações Comunitárias Rurais e com infraestrutura de trabalho em cada
+						localidade, passando pela produção, beneficiamentos dos produtos e comercialização.
+						</h5>
+						<h5>
+						Os cooperados estão dispostos nos quatro cantos da zona rural, sendo parte de ação integrada, 
+						que conta com o apoio das respectivas Associações Comunitárias Rurais e com infraestrutura em cada localidade.
+						</h5>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>	
+	<!-- end HISTORIA -->
+
+	<!-- team section -->
+	<div class="mt-150">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="section-title">
+						<h3>Os <span class="orange-text">Cooperados</span></h3>
+						<p> Somos compostos por mais de 40 quintais produtivos na zona rural de Feira de Santana - BA
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-4 col-md-6">
+					<div class="single-team-item">
+						<div class="team-bg team-bg-1"></div>
+						<h4>Jimmy Doe <span>Farmer</span></h4>
+						
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6">
+					<div class="single-team-item">
+						<div class="team-bg team-bg-2"></div>
+						<h4>Marry Doe <span>Farmer</span></h4>
+						
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 offset-md-3 offset-lg-0">
+					<div class="single-team-item">
+						<div class="team-bg team-bg-3"></div>
+						<h4>Simon Joe <span>Farmer</span></h4>
+						
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- end team section -->
+
+	<!-- FOOTER -->
+	<?php require_once("src/components/footer.php");?>
+
+	<!-- jquery -->
+	<script src="assets/js/jquery-1.11.3.min.js"></script>
+	<!-- bootstrap -->
+	<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+	<!-- count down -->
+	<script src="assets/js/jquery.countdown.js"></script>
+	<!-- isotope -->
+	<script src="assets/js/jquery.isotope-3.0.6.min.js"></script>
+	<!-- waypoints -->
+	<script src="assets/js/waypoints.js"></script>
+	<!-- owl carousel -->
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<!-- magnific popup -->
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
+	<!-- mean menu -->
+	<script src="assets/js/jquery.meanmenu.min.js"></script>
+	<!-- sticker js -->
+	<script src="assets/js/sticker.js"></script>
+	<!-- main js -->
+	<script src="assets/js/main.js"></script>
+
+</body>
+</html>
