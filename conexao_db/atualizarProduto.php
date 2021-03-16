5188e64d30d8f35454463abc4634877d6004d82a
amandabriena <amanda.briena@gmail.com>
2021-03-15 22:38:58 -0300

    ajustes finalizados do atualizar produto

--- conexao_db/atualizarProduto.php
+++ conexao_db/atualizarProduto.php
@@ -1,3 +1,59 @@
-<?php 
+<?php
+    include_once('conexao.php');
 
+    include_once('incluirImagem.php');
+    $id_produto = $_POST['id_produto'];
+    $nome = $_POST['nome'];
+    $descricao = $_POST['descricao'];
+    $preco = $_POST['preco'];
+    str_replace(',', '.', $preco);
+    $preco = floatval ($preco);
+    $categoria = $_POST['categoria'];
+    $imagem = $_POST['imagem'];
+
+    //se a categoria não estiver cadastrada, é realizado o cadastro da mesma
+    if($categoria == 'Outra'){
+        $categoria = $_POST['outraCat'];
+        //inserindo nova categoria
+        $queryCategoria = "INSERT INTO categoria_produto(id_categoria, nome) 
+        VALUES(NULL,'$categoria')";
+        $resultado = mysqli_query($connect, $queryCategoria);
+        //buscando id da nova categoria cadastrada
+        $resultado = mysqli_query($connect, "SELECT `AUTO_INCREMENT` FROM INFORMATION_SCHEMA.TABLES 
+        WHERE TABLE_SCHEMA = 'db_coobaf' AND TABLE_NAME = 'categoria_produto';");
+
+       $categoria = mysqli_fetch_assoc($resultado);
+       $categoria = (int)$categoria['AUTO_INCREMENT'] - 1;
+    }
+
+    //verificando se o produto está ativo ou não
+    if(isset($_POST['status'])){
+        $status = 1;
+    }else{
+        $status = 0;
+    }
+
+    //Inclusao da Imagem do produto
+    $arquivo_tmp = $_FILES[ 'arquivo' ][ 'tmp_name' ];
+    $nome_arq = $_FILES[ 'arquivo' ][ 'name' ];
+    $destino = incluirImagem($nome_arq, $arquivo_tmp);
+
+    if($destino == 0){
+        $destino = $imagem;
+    }
+    $query = "UPDATE produto SET  nome = '$nome', descricao = '$descricao', imagem = '$destino', 
+    id_categoria = '$categoria', status = '$status'  where id_produto = '$id_produto'";
+
+    $query = mysqli_query($connect,$query);
+    if($query){
+        $_SESSION['mensagem'] = "atualizar";
+        header('location:../ger_produtos.php');
+        //echo $categoria;
+    }else{
+        echo mysqli_error($connect);
+        //header('location:../404.php');
+    }
+    
+    
+    
 ?>
--- ger_produtos.php
+++ ger_produtos.php
@@ -108,21 +108,6 @@
 						}
 					?>
 			</div>
-				
-
-			<div class="row">
-				<div class="col-lg-12 text-center">
-					<div class="pagination-wrap">
-						<ul>
-							<li><a href="#">Prev</a></li>
-							<li><a href="#">1</a></li>
-							<li><a class="active" href="#">2</a></li>
-							<li><a href="#">3</a></li>
-							<li><a href="#">Next</a></li>
-						</ul>
-					</div>
-				</div>
-			</div>
 
 			<div class = " row text-left mb-80">
 				<a href="cooperado.php" class="cart-btn"><i class="fa fa-arrow-left"></i> Voltar</a>
