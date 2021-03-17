54a00375e14324b7023a20ba29d063007744d2f3
IanaAleixo <75858330+IanaAleixo@users.noreply.github.com>
2021-03-16 22:55:53 -0300

    adicionar carrinho com verificação de igualdade

--- conexao_db/adicionarCarrinho.php
+++ conexao_db/adicionarCarrinho.php
@@ -1,7 +1,13 @@
 <?php
 
 function adicionar_produto($id_produto){
-    $_SESSION['carrinho'][] = $id_produto;
+    foreach ($_SESSION['carrinho'] as &$value) {
+        if ($id_produto == $value){
+            //adicionar mais um na qnt
+        }else $_SESSION['carrinho'][] = $id_produto;
+    }
+
+    
 
 }
 
