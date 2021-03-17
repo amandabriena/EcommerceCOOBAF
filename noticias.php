8c187f430b631da88c65f30f35e32203bcbb8096
IanaAleixo <75858330+IanaAleixo@users.noreply.github.com>
2021-03-16 22:55:08 -0300

    redimensionar img ainda em loop

--- noticias.php
+++ noticias.php
@@ -58,22 +58,22 @@
 					$largura = "300";
 					echo "Altura pretendida: $altura - largura pretendida: $largura <br>";
 							$imagem_temporaria = imagecreatefromjpeg($row['imagem']);
-							//echo $row['imagem'];
+							echo $row['imagem'];
 							
-							//$largura_original = imagesx($imagem_temporaria);
+							$largura_original = imagesx($imagem_temporaria);
 							
-							//$altura_original = imagesy($imagem_temporaria);
+							$altura_original = imagesy($imagem_temporaria);
 							
-							//echo "largura original: $largura_original - Altura original: $altura_original <br>";
+							echo "largura original: $largura_original - Altura original: $altura_original <br>";
 							
-							//$nova_largura = $largura ? $largura : floor (($largura_original / $altura_original) * $altura);
+							$nova_largura = $largura ? $largura : floor (($largura_original / $altura_original) * $altura);
 							
-							//$nova_altura = $altura ? $altura : floor (($altura_original / $largura_original) * $largura);
+							$nova_altura = $altura ? $altura : floor (($altura_original / $largura_original) * $largura);
 							
-							//$imagem_redimensionada = imagecreatetruecolor($nova_largura, $nova_altura);
-							//imagecopyresampled($imagem_redimensionada, $imagem_temporaria, 0, 0, 0, 0, $nova_largura, $nova_altura, $largura_original, $altura_original);
+							$imagem_redimensionada = imagecreatetruecolor($nova_largura, $nova_altura);
+							imagecopyresampled($imagem_redimensionada, $imagem_temporaria, 0, 0, 0, 0, $nova_largura, $nova_altura, $largura_original, $altura_original);
 							
-							//imagejpeg($imagem_redimensionada, 'assets/img-upload/' . $row['imagem']);
+							imagejpeg($imagem_redimensionada, 'assets/img-upload/' . $row['imagem']);
 							
 							//echo "<img src='arquivo/".$_FILES['arquivo']['name']."'>";
 							
