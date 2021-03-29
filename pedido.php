5a59c66b70701f7d20940074aebb9f0a44aac182
amandabriena <amanda.briena@gmail.com>
2021-03-28 22:44:07 -0300

    chat pedido - nao finalizado

--- pedido.php
+++ pedido.php
@@ -17,6 +17,7 @@
 <html lang="en">
 <head>
 	<?php require_once("src/components/head.php");?>
+	<link rel="stylesheet" href="assets/css/chat.css">
 	<!-- title -->
 	<title>Pedido de Compra</title>
 	
@@ -59,21 +60,79 @@
 				</div>
 				<div class="col-lg-4 col-md-12 offset-lg-1">
 					<h4><strong>Contato com a COOBAF-FS</strong></h4>
-					<?php
-						if($row_pedido['status'] == 2){
-							echo "<img src='assets\img\img-status-pedido\status_01.png'>";
-							echo "<h6>Pedido realizado!</h6>";
-						}else if($row_pedido['status'] == 3){
-							echo "<img src='assets\img\img-status-pedido\status_02.png'>";
-							echo "<h6>Pedido em tramitação! Contato com a COOBAF-FS para que a entrega seja realizada!</h6>";
-						}else if($row_pedido['status'] == 4){
-							echo "<img src='assets\img\img-status-pedido\status_03.png'>";
-							echo "<h6>Pedido finalizado!</h6>";
-						}else{
-							echo "<img src='assets\img\img-status-pedido\status_04.png'>";
-							echo "<h6>Pedido cancelado :(</h6>";
-						}
-					?>
+				<div class="panel panel-primary">
+                <div class="panel-body">
+                    <ul class="chat">
+                        <li class="left clearfix"><span class="chat-img pull-left">
+                            <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="right clearfix"><span class="chat-img pull-right">
+                            <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                    <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="left clearfix"><span class="chat-img pull-left">
+                            <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="pull-right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="right clearfix"><span class="chat-img pull-right">
+                            <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" />
+                        </span>
+                            <div class="chat-body clearfix">
+                                <div class="header">
+                                    <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>15 mins ago</small>
+                                    <strong class="pull-right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="panel-footer">
+                    <div class="input-group">
+						<textarea id="btn-input" type="textarea" class="form-control input-sm" placeholder="Digite aqui..">
+						</textarea>
+                        
+                        <span class="input-group-btn">
+                            <button class="btn btn-success" id="btn-chat">
+                                Enviar Mensagem</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
 				</div>
 				<div class="col-lg-4 col-md-12 offset-lg-1">
 					<button type="button" class="btn btn-success" data-toggle="collapse" href="#collapseDetalhes" aria-controls="collapseDetalhes">Detalhes do Pedido</button>
